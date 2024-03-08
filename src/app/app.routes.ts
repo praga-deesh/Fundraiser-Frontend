b811c2b8c538ba815994ed24147328011cdcd5c1
K J <pragadeeshclone@gmail.com>
2024-03-08 20:40:54 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'posts', component: PostsComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    
+    { path: '**', component: PageNotFoundComponent }
+];
