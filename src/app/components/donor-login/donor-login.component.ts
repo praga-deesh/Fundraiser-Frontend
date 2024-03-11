6d91e7a46ecbff31a2e677b20016d4d5b7eeb9e4
Ashmithaa-23 <aannadu1@ford.com>
2024-03-11 15:21:31 +0530

    final

---
+++ src/app/components/donor-login/donor-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donor-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './donor-login.component.html',
+  styleUrl: './donor-login.component.css'
+})
+export class DonorLoginComponent {
+
+}
