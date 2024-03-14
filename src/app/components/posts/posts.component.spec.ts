a5e04ce8e5118ab8620b55b5895274a97fa41c8a
kishore010602 <kishore010602@gmail.com>
2024-03-14 20:10:41 +0530

    View posts and filters added

--- src/app/components/posts/posts.component.spec.ts
+++ src/app/components/posts/posts.component.spec.ts
@@ -1,23 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// Assuming you have PostService.spec.ts and Post.ts
 
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { PostsComponent } from './posts.component';
+import { PostService } from '../../services/post.service'; // Import PostService
+import { Post } from '../../model/post'; // Import Post model
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
+  let postService: PostService;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PostsComponent]
+    TestBed.configureTestingModule({
+      declarations: [ PostsComponent ],
+      providers: [ PostService ]
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PostsComponent);
+    .compileComponents(); // Compile components asynchronously
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PostsComponent); // Use TestBed.createComponent
     component = fixture.componentInstance;
+    postService = TestBed.inject(PostService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
