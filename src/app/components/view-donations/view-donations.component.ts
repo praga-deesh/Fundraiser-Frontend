ca2bf30c414af2e20f3a81705d64da21a6755023
K J <pragadeeshclone@gmail.com>
2024-03-21 16:47:16 +0530

    changes made

--- src/app/components/view-donations/view-donations.component.ts
+++ src/app/components/view-donations/view-donations.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './view-donations.component.css'
 })
 export class ViewDonationsComponent {
+  post: any;
+  user: any;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+        // Get user details from sessionStorage
+        let userData = sessionStorage.getItem('user');
+        if (userData) {
+          this.user = JSON.parse(userData);
+          console.log(this.user.id + "**");
+          let postData = sessionStorage.getItem('post');
+          if (postData) {
+            this.post = JSON.parse(postData);
+            console.log(this.post.id + "**");
+          }
+        }
+
+  }
 
 }
