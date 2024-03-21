bad555baae2185ec3e7ee8cca018f19014e7239f
K J <pragadeeshclone@gmail.com>
2024-03-21 18:09:31 +0530

    view-donations added

--- src/app/components/view-donations/view-donations.component.ts
+++ src/app/components/view-donations/view-donations.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewDonationsService } from '../../services/view-donations.service';
+import { Commentt } from '../../model/commentt';
+import { Donation } from '../../model/donation';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Donor } from '../../model/donor';
+import { DonorService } from '../../services/donor.service';
+
 
 @Component({
   selector: 'app-view-donations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './view-donations.component.html',
   styleUrl: './view-donations.component.css'
 })
@@ -11,8 +20,22 @@ export class ViewDonationsComponent {
   post: any;
   user: any;
 
-  constructor() {
+  message:string = "";
+  errorMessage:string = "";
+  
+  commentts: Commentt[] = [];
 
+  commentt:Commentt = new Commentt();
+
+  donations: Donation[] = [];
+
+  donation:Donation = new Donation();
+
+  donors:Donor[] = [];
+  
+  donor:Donor = new Donor();
+
+  constructor(private viewDonationService:ViewDonationsService,private donorService:DonorService,private router:Router) {
   }
 
   ngOnInit() {
@@ -28,6 +51,32 @@ export class ViewDonationsComponent {
           }
         }
 
+        this.getDonations(this.post.id);
+        this.getComments(this.post.id);
+
+
   }
 
+  getDonations(postId:number) {
+      if(postId) {
+        this.viewDonationService.viewDonations(postId)
+        .subscribe(donations => this.donations = donations);
+   
+      }
+      else {
+        this.errorMessage="No donations to Show ";
+      }
+    }
+
+    getComments(postId:number) {
+      if(postId) {
+        this.viewDonationService.viewComments(postId)
+        .subscribe(commentts => this.commentts = commentts);
+
+        
+      }
+      else {
+        this.errorMessage="No Comments to Show";
+      }
+    }
 }
